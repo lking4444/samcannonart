43198012714082a26f4a19b6dec7d95448e8d844
Louis King <thelouisking123@gmail.com>
2025-11-08 13:37:15 +0000

    Added the first component structure to the project

---
+++ myapp/app/Components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+
+export default function Header() {
+    const thing = "hello"
+
+    return (
+        <div className={styles.title}>
+            (hello) 
+        </div>
+    )
+}
+
